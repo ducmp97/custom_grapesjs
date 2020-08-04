3d9fcf8b98e1bf3d063798a6aaca2d34c885d0f8
ducmp97 <minhduc97.ptit@gmail.com>
2020-08-04 15:37:24 +0700

    create library

--- grapesjs/src/code_manager/view/EditorView.js
+++ grapesjs/src/code_manager/view/EditorView.js
@@ -19,6 +19,9 @@ export default Backbone.View.extend({
     this.$el.html(this.template(obj));
     this.$el.attr('class', this.pfx + 'editor-c');
     this.$el.find('#' + this.pfx + 'code').append(this.model.get('input'));
+    console.log('HTML', this.config.em.config.components);
+    console.log('THAT', this);
     return this;
+
   }
 });
